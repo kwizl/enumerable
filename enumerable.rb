31bddafad563750837a85b98399af219d0810ba6
kwizl <karis.njoroge@gmail.com>
2020-02-19 02:40:54 +0300

    Rectified rubocop errors

--- enumerable.rb
+++ enumerable.rb
@@ -1,17 +1,18 @@
-module Enumerable
+# frozen_string_literal: true
 
+# Custom Enumerable methods
+module Enumerable
   def my_each
-    for i in 0..self.length-1
-        yield(self[i])
+    for i in 0..length - 1
+      yield(self[i])
     end
     self
   end
-
 end
 
 arr = [2, 3, 4, 5]
 
 arr.my_each do |i|
-  print i 
+  print i
 end
 puts
