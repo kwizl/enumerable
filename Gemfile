8aa02a32d6f09b8922d51c66d3092758b7f81f96
kwizl <karis.njoroge@gmail.com>
2020-02-19 02:58:56 +0300

    Created my_each_with_index method

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 source 'https://rubygems.org'
 git_source(:github) { "https://github.com/#{repo_name}" }
 gem 'rubocop'
--- enumerable.rb
+++ enumerable.rb
@@ -1,17 +1,30 @@
-module Enumerable
+# frozen_string_literal: true
 
+# Custom Enumerable Methods
+module Enumerable
   def my_each
-    for i in 0..self.length-1
-        yield(self[i])
+    for i in 0..length - 1
+      yield(self[i])
     end
     self
   end
 
+  def my_each_with_index
+    for i in 0..length - 1
+      yield(self[i], i)
+    end
+    self
+  end
 end
 
 arr = [2, 3, 4, 5]
 
 arr.my_each do |i|
-  print i 
+  p i
+end
+puts
+
+arr.my_each_with_index do |v, k|
+  p "#{k}: #{v}"
 end
 puts
