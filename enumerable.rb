b94524b218f79fa13781b6b34eede15984e90844
kwizl <karis.njoroge@gmail.com>
2020-02-19 19:21:54 +0300

    Created custom my_select method

--- enumerable.rb
+++ enumerable.rb
@@ -15,16 +15,28 @@ module Enumerable
     end
     self
   end
+
+  def my_select
+    array = []
+    my_each do |i|
+      array.push(i) if yield(i)
+    end
+    array
+  end
 end
 
-arr = [2, 3, 4, 5]
+arr = [2, 3, 8, 5, 7, 10]
 
 arr.my_each do |i|
-  p i
+  print i
 end
+puts
 puts
 
 arr.my_each_with_index do |v, k|
   p "#{k}: #{v}"
 end
+puts
+
+p(arr.my_select { |i| i > 4 })
 puts
