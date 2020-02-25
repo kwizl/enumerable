d56c73787f15660eb594c10d2c9d94314e46417c
kwizl <karis.njoroge@gmail.com>
2020-02-25 10:05:02 +0300

    Add the ablility to loop over Range

--- enumerable.rb
+++ enumerable.rb
@@ -3,18 +3,30 @@
 # Custom Enumerable methods
 module Enumerable
   def my_each
-    count = 0
-    while count < self.length
-      yield(self[count])
-      count += 1
+    if self.class == Range
+      l = self.to_a
+      for i in 0...l.length
+        yield(l[i])
+      end
+      l
+    else
+      count = 0
+      while count < self.length
+        yield(self[count])
+        count += 1
+      end
+      self
     end
-    self
   end
 end
 
 arr = [2, 3, 4, 5]
 
 arr.my_each do |i|
+  print i
+end
+puts
+(0..2).my_each do |i|
   print i
 end
 puts
