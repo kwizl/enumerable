12c8c97c8652b72d11f7acb360fd6aee1d3d6ab3
kwizl <karis.njoroge@gmail.com>
2020-02-25 11:16:48 +0300

    Testing stylelint

--- Gemfile
+++ Gemfile
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 source 'https://rubygems.org'
 git_source(:github) { "https://github.com/#{repo_name}" }
 gem 'rubocop'
--- enumerable.rb
+++ enumerable.rb
@@ -1,6 +1,3 @@
-# frozen_string_literal: true
-# rubocop:disable Style/LineLength, Style/StringLiterals
-# Custom Enumerable Methods
 module Enumerable
   def my_each
     if self.class == Range
